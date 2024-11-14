a072d2e305e19b3637d40ee65b4924c5c85141ef
Denis Kolosov <d.kolosov@apibank.ru>
2024-11-14 14:54:54 +0700

    Refactor task

--- src/users/tasks.py
+++ src/users/tasks.py
@@ -14,27 +14,20 @@ logger = structlog.get_logger(__name__)
 def process_event_outbox() -> None:
     with start_transaction(op="task", name="Process Event Outbox"):
         logger.info("Processing event outbox")
-
         try:
             with transaction.atomic():
                 # Use skip_locked to avoid deadlocks
-                unprocessed_events = list(
-                    EventOutbox.objects.select_for_update(
-                        skip_locked=True,
-                    ).filter(processed=False).values("id", "event_context", "event_type"),
-                )
-                if not unprocessed_events:
+                unprocessed_events = EventOutbox.objects.select_for_update(
+                    skip_locked=True,
+                ).filter(processed=False).values("id", "event_context", "event_type")
+
+                if not unprocessed_events.exists():
                     logger.info("No unprocessed events")
                     return
-                logger.info(f"Found {len(unprocessed_events)} unprocessed events")
-                try:
-                    batch_insert_into_clickhouse(unprocessed_events)
-                except Exception as batch_exc:
-                    logger.exception(f"Failed to insert into CH, will not commit to EventOutbox: {batch_exc}")
-                    # Rollback if exception raised
-                    raise
+                logger.info(f"Found {unprocessed_events.count()} unprocessed events")
+                batch_insert_into_clickhouse(list(unprocessed_events))
 
                 EventOutbox.objects.filter(id__in=[event['id'] for event in unprocessed_events]).update(processed=True)
-                logger.info(f"Marked {len(unprocessed_events)} events as processed")
+                logger.info(f"Marked {unprocessed_events.count()} events as processed")
         except Exception as overall_exception:
             logger.exception(f"Transaction rolled back, error: {overall_exception}")
